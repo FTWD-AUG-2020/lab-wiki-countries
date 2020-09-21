1aac62d8a327ae4289796ccebdd94e6e01275f68
rqsell <68514875+rqsell@users.noreply.github.com>
2020-09-21 16:58:15 -0400

    rebecca's try

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar'
-import CountriesList from './components/CountriesList'
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <CountriesList />
+      {/* Create Switch  */}
+      <Switch>
+        <Route path="/:id" render={(props) => <CountryDetails {...props} />} />
+      </Switch>
     </div>
   );
 }
