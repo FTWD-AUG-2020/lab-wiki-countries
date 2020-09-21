eb8e6e549cf79fda70c668a605a18ed11b6b3148
Sebastian Grana <sebastiangrana1@gmail.com>
2020-09-21 17:24:12 -0400

    almost south africa

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import countryList from '../countries.json';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 function CountriesList() {
 
--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import countries from '../countries.json';
+import { Link } from 'react-router-dom';
 
 function CountryDetails(props) {
   //first map through countries
@@ -13,6 +14,14 @@ function CountryDetails(props) {
     <div className="flag">
       {country.name.common}
       {country.flag}
+      <ul>
+      {country.borders.map(border => {
+        let foundCountry = countries.find(country => country.cca3 === border)
+        return(
+          <li><Link to={`/${border}`}>{foundCountry.name.common}</Link></li>
+        );
+      })}
+      </ul>
     </div>
   );
 }
