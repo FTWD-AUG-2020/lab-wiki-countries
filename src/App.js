2966c1cd3b3de673f05beb9ce0b799340eaceb09
matheus <mtava002@gmail.com>
2020-09-21 16:34:40 -0400

    Matheus

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar'
+import CountriesList from './components/CountriesList'
 
 function App() {
   return (
     <div className="App">
-     <Navbar />
+      <Navbar />
+      <CountriesList />
     </div>
   );
 }
--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import countryList from '../countries.json';
+import { Link } from 'react-router-dom'
 
-function CountriesList(props) {
+function CountriesList() {
+
+  let listOfLinks = countryList.map(country => {
+    return (
+      <li><Link to={`/${country.cca3}`}>{country.name.common}</Link></li>
+    )
+  })
   return (
     <div>
-      
+      <ul>{listOfLinks}</ul>
     </div>
   );
 }
